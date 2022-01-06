01bd2e48aa7087222c68b83c98b7126da1e490a6
MainaGeorge <nuriszka@gmail.com>
2022-01-06 10:17:26 +0100

    overriding SaveChanges to include automatic audit when changing data in the database

--- ActivityThree/Program.cs
+++ ActivityThree/Program.cs
@@ -8,11 +8,30 @@ static class Program
 {
     private static IConfigurationRoot configurationRoot;
     private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
+    //simulate a user in the system
+    private const string _systemUserId = "2fd28110-93d0-427d-9207-d55dbca680fa";
+
+    //simulate a logged in user who is making changes to the db
+    private const string _loggedInUserId = "e2eb8989-a81a-4151-8e86-eb95a7961da2";
+
     static void Main()
     {
         BuildOptions();
+        DeleteAllItems();
         EnsureItems();
+        UpdateItems();
         ListInventory();
+    }
+
+    private static void DeleteAllItems()
+    {
+        using var db = new InventoryDbContext(_optionsBuilder.Options);
+        var items = db.Items.ToList();
+
+        db.Items.RemoveRange(items);
+
+        db.SaveChanges();
     }
 
     static void BuildOptions()
@@ -25,20 +44,30 @@ static class Program
     }
     static void EnsureItems()
     {
-        EnsureItem("Batman Begins");
-        EnsureItem("Inception");
-        EnsureItem("The Matrix");
-        EnsureItem("The Matrix Revolutions");
-        EnsureItem("Top Gun");
+        EnsureItem("Batman Begins", "You either die the hero or live long enough to see yourself become the villain", "Christian Bale, Katie Holmes");
+        EnsureItem("Inception", "You mustn't be afraid to dream a little bigger, darling", "Leonardo DiCaprio, Tom Hardy, Joseph GordonLevitt");
+        EnsureItem("Remember the Titans", "Left Side, Strong Side", "Denzell Washington, Will Patton");
+        EnsureItem("Star Wars: The Empire Strikes Back", "He will join us or die, master", "Harrison Ford, Carrie Fisher, Mark Hamill");
+        EnsureItem("Top Gun", "I feel the need, the need for speed!", "Tom Cruise, Anthony Edwards, Val Kilmer");
     }
-    private static void EnsureItem(string name)
+
+    private static void EnsureItem(string name, string description, string notes)
     {
+        var random = new Random();
         using var db = new InventoryDbContext(_optionsBuilder.Options);
         var existingItem =  db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
 
         if(existingItem is null)
         {
-            var item = new Item { Name = name };
+            var item = new Item 
+            { 
+                Name = name,
+                Description=description, 
+                Notes=notes, IsActive=true, 
+                CreateByUserId=_loggedInUserId, 
+                Quantity=random.Next() 
+            };
+
             db.Items.Add(item);
             db.SaveChanges();
         }
@@ -50,6 +79,18 @@ static class Program
         var items = db.Items.OrderByDescending(i => i.Name).ToList();
 
         items.ForEach(i => Console.WriteLine($"Item : {i.Name}"));
+    }
+
+    static void UpdateItems()
+    {
+        using var db = new InventoryDbContext(_optionsBuilder.Options);
+        var items = db.Items.ToList();
+
+        items.ForEach(i => i.CurrentOrFinalPrice = 9.99M);
+        db.Items.UpdateRange(items);
+
+        db.SaveChanges();
+        
     }
 }
 
--- EFCore_Library/InventoryDbContext.cs
+++ EFCore_Library/InventoryDbContext.cs
@@ -7,6 +7,8 @@ namespace EFCore_Library
     public class InventoryDbContext : DbContext
     {
         private static IConfigurationRoot _configuration;
+        private const string _systemUserId = "2fd28110-93d0-427d-9207-d55dbca680fa";
+
         public DbSet<Item> Items { get; set; }
         public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options)
         {
@@ -14,6 +16,58 @@ namespace EFCore_Library
 
         public InventoryDbContext()
         {
+        }
+
+        public override int SaveChanges()
+        {
+            var tracker = ChangeTracker;
+
+            foreach(var entry in tracker.Entries())
+            {
+                if(entry.Entity is FullAuditModel auditmodel)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            auditmodel.CreatedDate = DateTime.Now;
+                            if(String.IsNullOrWhiteSpace(auditmodel.CreateByUserId))
+                                 auditmodel.CreateByUserId = _systemUserId;
+                            break;
+
+                        case EntityState.Modified:
+                        case EntityState.Deleted:
+                            auditmodel.LastModifiedDate = DateTime.Now;
+                            if(String.IsNullOrWhiteSpace(auditmodel.LastModifiedUserId))
+                               auditmodel.LastModifiedUserId = _systemUserId;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", reloadOnChange: true, optional: true);
+
+                _configuration = builder.Build();
+
+
+                var connectionString = _configuration.GetConnectionString("InventoryDatabase");
+
+                optionsBuilder.UseSqlServer(connectionString, settings =>
+                {
+                    settings.EnableRetryOnFailure();
+                });
+            }
         }
     }
 }
